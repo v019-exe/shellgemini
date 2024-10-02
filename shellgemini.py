175c612e7ac3f1feee8d8f445be28464d4f1e68a
v019-exe <v019.exe@gmail.com>
2024-10-02 20:08:46 +0200

    Update shellgemini.py

--- shellgemini.py
+++ shellgemini.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import requests
 import sys
 import configparser
 
-
 config = configparser.ConfigParser()
-config.read("shellgemini.conf")
+config.read("/usr/local/bin/shellgemini.conf")
 
 GEMINI_API_KEY = config['DEFAULT']['api_key']
-
-
 
 def chat_gemini(text: str):
     url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={GEMINI_API_KEY}"   
     headers = {
         "Content-Type": "application/json"
     }
-    data = {"contents":[{"parts":[{"text":f"{text}"}]}]}
+    data = {"contents": [{"parts": [{"text": f"{text}"}]}]}
     response = requests.post(url, headers=headers, json=data)
-    response_json = response.json()
-    return response_json["candidates"][0]["content"]["parts"][0]["text"]
+    if response.status_code == 400:
+        print("Clave API no valida")
+    else:
+        response_json = response.json()
+        return response_json["candidates"][0]["content"]["parts"][0]["text"]
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
