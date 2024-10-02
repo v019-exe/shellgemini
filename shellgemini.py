9440feba37566db5bdc30d70eb0e65ab5f2b2370
v019-exe <v019.exe@gmail.com>
2024-10-02 19:52:54 +0200

    Create shellgemini.py

---
+++ shellgemini.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read("shellgemini.conf")
+
+GEMINI_API_KEY = config['DEFAULT']['api_key']
+
+
+
+def chat_gemini(text: str):
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={GEMINI_API_KEY}"   
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {"contents":[{"parts":[{"text":f"{text}"}]}]}
+    response = requests.post(url, headers=headers, json=data)
+    response_json = response.json()
+    return response_json["candidates"][0]["content"]["parts"][0]["text"]
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Uso de shellgemini: shellgemini <pregunta>")
+        sys.exit(1)
+        
+    pregunta = " ".join(sys.argv[1:])
+    respuesta = chat_gemini(text=pregunta)
+    
+    print(respuesta)
